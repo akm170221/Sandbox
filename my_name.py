87d0e7623be7d8d84241906a1a4dce6b7ccd3b4e
Sandbox <aungkaungmyat@my.jcu.edu.au>
2021-04-10 22:43:35 +0800

    Check whether a user is an adult

--- my_name.py
+++ my_name.py
@@ -4,4 +4,12 @@ def main():
     name = input("Name? ")
     age = input("Age? ")
     print("Your name is {} and age is {}". format(name, age))
+    if is_adult(age):
+        print("You are an adult")
+    else:
+        print("You are a child")
+
+def is_adult(age):
+    return age >=21
+
 main()
